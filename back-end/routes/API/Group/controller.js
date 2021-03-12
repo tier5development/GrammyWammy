792446e9d421b197d1d59b2374bce55e35ad5e6d
Suvadeep <work@tier5.us>
2021-03-12 14:11:15 +0530

    Message Group Edit Part Done

--- back-end/routes/API/Group/controller.js
+++ back-end/routes/API/Group/controller.js
@@ -5,10 +5,9 @@ module.exports.createGroup = async (req, res) => {
         let mess="";
         let code=1;
         let messageGroup = [];
-        console.log("This is my sent",req.body);
-       
-        if(req.body.group_name !="" && req.body.user_id !="" ){
-            
+        //console.log("This is my sent",req.body);
+        
+        if(req.body.group_name !="" && req.body.user_id !=""  && req.body.BlockStorage !=""){
             let payload ={
                 user_id:req.body.user_id,
                 title:req.body.group_name,
@@ -75,8 +74,8 @@ module.exports.editGroup  =   async   (req,   res)    =>  {
     try{
         let mess="";
         let code=1;
-        if(req.body.segment_id !=""  ){
-            messageSegment=await MessageSegment.GetMessageSegment(req.body.segment_id);
+        if(req.body.group_id !=""  ){
+            MessageGroups=await MessageGroup.GetMessageGroup(req.body.group_id);
             mess="Segments List Successfully";
             code=1;
         }else{
@@ -86,7 +85,38 @@ module.exports.editGroup  =   async   (req,   res)    =>  {
         res.send({
             code: code,
             message: mess,
-            payload: messageSegment
+            payload: MessageGroups
+        })    
+    } catch (error) {
+        res.send({
+            code: 3,
+            message: error.message,
+            payload: error
+        })
+    }
+}
+module.exports.updateGroup  =   async   (req,   res)    =>  {
+    try{
+        let mess="";
+        let code=1;
+        if(req.body.group_id_edit !=""  ){
+            let payload ={
+                title:req.body.group_name_edit,
+                associate_blocks:req.body.BlockStorage
+            }
+            let updateMessageGroup=await MessageGroup.updateMessageGroupById(payload,req.body.group_id_edit);
+            messageGroup=await MessageGroup.GetAllMessageGroup(req.body.user_id);
+            //messageSegment=await MessageSegment.GetMessageSegment(req.body.sagment_id_edit);
+            mess="Segments List Successfully";
+            code=1;
+        }else{
+            mess="Segments List Un-Successfully";
+            code=2;
+        }
+        res.send({
+            code: code,
+            message: mess,
+            payload: messageGroup
         })    
     } catch (error) {
         res.send({
