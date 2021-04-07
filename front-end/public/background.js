9d41879e4c5906e2f55a9940c9c944ae2ea01147
Suvadeep <work@tier5.us>
2021-04-07 16:34:16 +0530

    All Issue Solved Regarding Default Message Posting

--- front-end/public/background.js
+++ front-end/public/background.js
@@ -210,13 +210,12 @@ const urlParam = "instaExt";
 chrome.runtime.onConnect.addListener(function(port) {
   //if (port.name === 'knockknock') {
     port.onMessage.addListener(async function(msg) {
-
-      if (msg.ConFlag == "CheckMessageContent")
+    if (msg.ConFlag == "CheckMessageContent")
       {
+              console.log('Reached Here');
               messageLink = msg.options.messageLink;
               messageId = messageLink.split("/").pop();
               messageUserName = msg.options.userName;
-              //setUserDetails(messageUserName);
               if(msg.options.messageContent == 'Typing...')
               {
                 chrome.windows.getCurrent(w => {
@@ -229,30 +228,19 @@ chrome.runtime.onConnect.addListener(function(port) {
                 });
                 return false;
               }
-              setTimeout(function () {
-              messageContent =  getAutoResponseText(msg.options.messageContent,messageUserName,messageId);
-            
-              // chrome.tabs.update( parseInt(localStorage.getItem("profileTabId")), { 
-              //   url: `https://www.instagram.com/direct/inbox/?id=${messageId}&message=${messageContent}&${urlParam}=true`,
-              //   active: true}, function(tab) {
-              //     tabId = tab.id;
-              //   });
-              
-              }, 1000);
-      
-      }
+              getAutoResponseText(msg.options.messageContent,messageUserName,messageId);
+        }
+
         function getAutoResponseText(message,userName,messageId)
         {
+          console.log('Reached The Function');
           var regex = new RegExp(/^(?!.*\.\.)(?!.*\.$)[^\W][\w.]{0,29}$/);
           var validation = regex.test(userName);
           if(validation) {
             $.get("https://www.instagram.com/"+userName+"/?__a=1")
             .done(function(data) { 
   
-             
               var userFullName = data["graphql"]["user"]["full_name"];
-            
-
               let autoResponderKeywords = JSON.parse(localStorage.getItem('keywordsTally'));
               let responseMessage ='';
                   for (var i = 0; i < autoResponderKeywords.length; i++) {
@@ -263,17 +251,10 @@ chrome.runtime.onConnect.addListener(function(port) {
                         console.log(`${individualKeyword} is the keyword`);
                         if (userMessage.includes(individualKeyword)) {
                             console.log('Condition Satisfied');
-                            //console.log('Details '+localStorage.getItem("individualMessageDetails"));
-                          // console.log('Username'+userName);
-                            
-                                //console.log('no stored');
                            
-                           
-                            //console.log(fullName+' Full Name');
                             let splitFullName = userFullName.split(" ");
                             let firstName = (splitFullName[0]) ? splitFullName[0] :'';
                             let lastName = (splitFullName[1]) ? (splitFullName[1]) : '';
-          
           
                             let ResponseText = object.message;
                             console.log(ResponseText);
@@ -381,7 +362,6 @@ chrome.runtime.onConnect.addListener(function(port) {
                             {
                               
                               port.postMessage({userInfoDetails: responsenewvalue.payload.message,ThreadParams:paramsToSend,ConFlagBack:"DEFAULTMESSAGEBACK" });
-                              //localStorage.setItem("defaultMessageFromBackend",responsenewvalue.payload.message);
                               var messageContent = responsenewvalue.payload.message;
                               chrome.tabs.update( parseInt(localStorage.getItem("profileTabId")), { 
                               url: `https://www.instagram.com/direct/inbox/?id=${messageId}&message=${messageContent}&${urlParam}=true`,
@@ -402,44 +382,10 @@ chrome.runtime.onConnect.addListener(function(port) {
                 } else {
                   console.log('The username is invalid!')
                 }
-          
-        }
-    
-        function setUserDetails(userName)
-        {
-          
-            var regex = new RegExp(/^(?!.*\.\.)(?!.*\.$)[^\W][\w.]{0,29}$/);
-            var validation = regex.test(userName);
-            if(validation) {
-              $.get("https://www.instagram.com/"+userName+"/?__a=1")
-              .done(function(data) { 
-    
-                var userImage = data["graphql"]["user"]["profile_pic_url_hd"];
-                var userFullName = data["graphql"]["user"]["full_name"];
-                var userId = data["graphql"]["user"]["id"];
-                var userName = data["graphql"]["user"]["username"];
-                let  params ={
-                  userId    :   userId,
-                  userFullName   :   userFullName,
-                  userName :   userName,
-                  userImage :   userImage
-                };
-                localStorage.setItem("individualMessageDetails",JSON.stringify(params));
-                
-                })
-              .fail(function() { 
-                // code for 404 error 
-                console.log('Username was not found!')
-              })
-            
-            } else {
-              console.log('The username is invalid!')
             }
-        }
-
-
-        if(msg.ConFlag == "STOREANDCLOSE"){
-          let params  =   {
+    
+           if(msg.ConFlag == "STOREANDCLOSE"){
+            let params  =   {
             FacebookFirstName : msg.MessageDetails.FacebookFirstName,
             FacebookLastName  :msg.MessageDetails.FacebookLastName,
             FacebookUserId  :msg.MessageDetails.FacebookUserId,
@@ -461,28 +407,10 @@ chrome.runtime.onConnect.addListener(function(port) {
             individualThreadList.splice(indexthreadlink, 1);
             let NewListURLArray=JSON.stringify(individualThreadList);
             localStorage.setItem('ListURLArray', NewListURLArray);
-            //document.getElementById('messageIndividualMain').src ="";
             localStorage.setItem('CheckMessageNReply',0);
             CheckLocalStoreAndHitIndividualMList();
           }
           
-          // if(individualThreadList.length != 0){
-          //   let NewIndividualThreadLinksx = [];
-          //   let i=0;
-          //   await individualThreadList.map(async function(eachval){
-          //     if(eachval==msg.MessageDetails.LocationDetails){
-                
-          //     }else{
-          //       NewIndividualThreadLinksx[i]=msg.MessageDetails.LocationDetails;
-          //       i=i+1;
-          //     }
-          //   });
-          //   document.getElementById('messageIndividualMain').src ="";
-          //   localStorage.setItem('ListURLArray',NewIndividualThreadLinksx);
-          //   localStorage.setItem('CheckMessageNReply',0);
-          //   CheckLocalStoreAndHitIndividualMList();
-          // }
-    
           console.log("Now  Again I am In BackGround 332",msg.MessageDetails);
           console.log("Now  i have  to send this in db from BackGround 344",params);
         }  
