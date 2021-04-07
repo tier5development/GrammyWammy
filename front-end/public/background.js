c1449733a1e3ef05b6fd22478ce435c79086b6d0
Suvadeep <work@tier5.us>
2021-04-07 14:08:58 +0530

    Default Message Time Interval Implementation V1.1

--- front-end/public/background.js
+++ front-end/public/background.js
@@ -8,6 +8,7 @@ const getUserToken = () => localStorage.getItem("kyubi_user_token");
  * 
 */
 const handleRequest = (path, methodType, bodyData) => {
+   
     let getWithCredentialHeader = {
         'Accept': 'application/json', 'Content-Type': 'application/json','Access-Control-Allow-Origin': true
     };
@@ -215,7 +216,7 @@ chrome.runtime.onConnect.addListener(function(port) {
               messageLink = msg.options.messageLink;
               messageId = messageLink.split("/").pop();
               messageUserName = msg.options.userName;
-              setUserDetails(messageUserName);
+              //setUserDetails(messageUserName);
               if(msg.options.messageContent == 'Typing...')
               {
                 chrome.windows.getCurrent(w => {
@@ -229,136 +230,178 @@ chrome.runtime.onConnect.addListener(function(port) {
                 return false;
               }
               setTimeout(function () {
-              messageContent =  getAutoResponseText(msg.options.messageContent,messageUserName);
+              messageContent =  getAutoResponseText(msg.options.messageContent,messageUserName,messageId);
             
-              chrome.tabs.update( parseInt(localStorage.getItem("profileTabId")), { 
-                url: `https://www.instagram.com/direct/inbox/?id=${messageId}&message=${messageContent}&${urlParam}=true`,
-                active: true}, function(tab) {
-                  tabId = tab.id;
-                });
+              // chrome.tabs.update( parseInt(localStorage.getItem("profileTabId")), { 
+              //   url: `https://www.instagram.com/direct/inbox/?id=${messageId}&message=${messageContent}&${urlParam}=true`,
+              //   active: true}, function(tab) {
+              //     tabId = tab.id;
+              //   });
               
               }, 1000);
       
       }
-        function getAutoResponseText(message,userName)
+        function getAutoResponseText(message,userName,messageId)
         {
+          var regex = new RegExp(/^(?!.*\.\.)(?!.*\.$)[^\W][\w.]{0,29}$/);
+          var validation = regex.test(userName);
+          if(validation) {
+            $.get("https://www.instagram.com/"+userName+"/?__a=1")
+            .done(function(data) { 
+  
+             
+              var userFullName = data["graphql"]["user"]["full_name"];
+            
+
+              let autoResponderKeywords = JSON.parse(localStorage.getItem('keywordsTally'));
+              let responseMessage ='';
+                  for (var i = 0; i < autoResponderKeywords.length; i++) {
+                    let object = autoResponderKeywords[i];
+                        let userMessage = message.toLowerCase();
+                        let individualKeyword = object.keyword.toLowerCase();
+                        console.log(`${userMessage} is the usermessage`);
+                        console.log(`${individualKeyword} is the keyword`);
+                        if (userMessage.includes(individualKeyword)) {
+                            console.log('Condition Satisfied');
+                            //console.log('Details '+localStorage.getItem("individualMessageDetails"));
+                          // console.log('Username'+userName);
+                            
+                                //console.log('no stored');
+                           
+                           
+                            //console.log(fullName+' Full Name');
+                            let splitFullName = userFullName.split(" ");
+                            let firstName = (splitFullName[0]) ? splitFullName[0] :'';
+                            let lastName = (splitFullName[1]) ? (splitFullName[1]) : '';
+          
+          
+                            let ResponseText = object.message;
+                            console.log(ResponseText);
+                            console.log(firstName);
+                            console.log(lastName);
+                            let NowTime=new Date().getTime();  
+                            let a = new Date(NowTime);
+                            let months = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'];
+                            let year = a.getFullYear();
+                            let month = months[a.getMonth()];
+                            let date = a.getDate();
+                          
+                            let OnlyDate = date + ' ' + month + ' ' + year ;
+                            let NewResponseText = ResponseText.replace('{first_name}',firstName);
+                            NewResponseText = NewResponseText.replace('{last_name}',lastName);
+                            NewResponseText = NewResponseText.replace('{Date}',OnlyDate);
+                            NewResponseText = NewResponseText.replace('{date}',OnlyDate);
+                            responseMessage += NewResponseText;
+                        } 
+                  }
+                  if(responseMessage)
+                  {
+                      chrome.tabs.update( parseInt(localStorage.getItem("profileTabId")), { 
+                      url: `https://www.instagram.com/direct/inbox/?id=${messageId}&message=${responseMessage}&${urlParam}=true`,
+                      active: true}, function(tab) {
+                        tabId = tab.id;
+                      });
+                  }
+                  else
+                  {
+                    getDefaultMessage(userName,messageId);
+                    return localStorage.getItem('defaultMessageFromBackend');
+                  }
+              
+              
+              })
+            .fail(function() { 
+              // code for 404 error 
+              console.log('Username was not found!')
+            })
+          
+          } else {
+            console.log('The username is invalid!')
+          }
            
-            let autoResponderKeywords = JSON.parse(localStorage.getItem('keywordsTally'));
-            let responseMessage ='';
-            for (var i = 0; i < autoResponderKeywords.length; i++) {
-              let object = autoResponderKeywords[i];
-                  let userMessage = message.toLowerCase();
-                  let individualKeyword = object.keyword.toLowerCase();
-                  console.log(`${userMessage} is the usermessage`);
-                  console.log(`${individualKeyword} is the keyword`);
-                  if (userMessage.includes(individualKeyword)) {
-                      console.log('Condition Satisfied');
-                      //console.log('Details '+localStorage.getItem("individualMessageDetails"));
-                     // console.log('Username'+userName);
-                      if(localStorage.getItem("individualMessageDetails"))
-                      {
-                          //console.log('stored');
-                          fullName = JSON.parse(localStorage.getItem("individualMessageDetails")).userFullName;
-                      }
-                      else
-                      {
-                          //console.log('no stored');
-                          fullName = userName;
-                      }
-                      //console.log(fullName+' Full Name');
-                      let splitFullName = fullName.split(" ");
-                      let firstName = (splitFullName[0]) ? splitFullName[0] :'';
-                      let lastName = (splitFullName[1]) ? (splitFullName[1]) : '';
-    
-    
-                      let ResponseText = object.message;
-                      console.log(ResponseText);
-                      console.log(firstName);
-                      console.log(lastName);
-                      let NowTime=new Date().getTime();  
-                      let a = new Date(NowTime);
-                      let months = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'];
-                      let year = a.getFullYear();
-                      let month = months[a.getMonth()];
-                      let date = a.getDate();
-                     
-                      let OnlyDate = date + ' ' + month + ' ' + year ;
-                      let NewResponseText = ResponseText.replace('{first_name}',firstName);
-                      NewResponseText = NewResponseText.replace('{last_name}',lastName);
-                      NewResponseText = NewResponseText.replace('{Date}',OnlyDate);
-                      NewResponseText = NewResponseText.replace('{date}',OnlyDate);
-                      responseMessage += NewResponseText;
-                  } 
-            }
-            if(responseMessage)
-            {
-               return responseMessage;
-            }
-            else
-            {
-              getDefaultMessage();
-              return localStorage.getItem('defaultMessageFromBackend');
-            }
         }
 
 
-        function getDefaultMessage()
+        function getDefaultMessage(userName,messageId)
         {
-          console.log('individual Message Details '+localStorage.getItem('individualMessageDetails'));
-          let GrammyWammyId  = localStorage.getItem('user_id');
-          let NowTime=new Date().getTime();  
-          if(localStorage.getItem("individualMessageDetails"))
-          {
-                messagePostedBy  = JSON.parse(localStorage.getItem("individualMessageDetails"));
-                fullName = messagePostedBy.userFullName;
-                let splitFullName = fullName.split(" ");
-                let firstName = (splitFullName[0]) ? splitFullName[0] :'';
-                let lastName = (splitFullName[1]) ? (splitFullName[1]) : '';
-                let postedByUsername = messagePostedBy.userName;
-                let profileLink = `https://www.instagram.com/${postedByUsername}`;
-                let instagramFriendId = messagePostedBy.userId;
+                var regex = new RegExp(/^(?!.*\.\.)(?!.*\.$)[^\W][\w.]{0,29}$/);
+                var validation = regex.test(userName);
+                if(validation) {
+                  $.get("https://www.instagram.com/"+userName+"/?__a=1")
+                  .done(function(data) { 
+        
+                        var userImage = data["graphql"]["user"]["profile_pic_url_hd"];
+                        var userFullName = data["graphql"]["user"]["full_name"];
+                        var userId = data["graphql"]["user"]["id"];
+                        var userName = data["graphql"]["user"]["username"];
+                        
+                        let GrammyWammyId  = localStorage.getItem('user_id');
+                        let NowTime=new Date().getTime();  
 
+                        let splitFullName = userFullName.split(" ");
+                        let firstName = (splitFullName[0]) ? splitFullName[0] :'';
+                        let lastName = (splitFullName[1]) ? (splitFullName[1]) : '';
+                        let postedByUsername = userName;
+                        let profileLink = `https://www.instagram.com/${userName}`;
+                        let instagramFriendId = userId;
+
+                        let paramsToSend  =   {
+                          MfenevanId:GrammyWammyId,
+                          FacebookUserId:localStorage.getItem('instagramIdForTheLoggedInUser'),
+                          FriendFacebookId:instagramFriendId,
+                          FacebookFirstName:firstName,
+                          FacebookLastName:lastName,
+                          ProfileLink:profileLink,
+                          Username: postedByUsername,
+                          TimeNow:NowTime
+                        }
+
+                          console.log('Params To Send '+firstName);
+
+                          let response  =  handleRequest(
+                          "api/friend/checkFriendReadyToReciveDefaultMessage",
+                          method.POST,
+                          toJsonStr(paramsToSend)
+                          ).then(async response =>  {
+                            let responsenewvalue = await response.json();
+                            console.log("Hit For Default",paramsToSend);
+                            console.log("Hit For Default Now Get From Backend",responsenewvalue.payload.message);
+                            console.log("The whole payload",responsenewvalue);
+                            
+                            if(responsenewvalue.code === 2)
+                            {
+                              var messageContent = 'Failed';
+                              chrome.tabs.update( parseInt(localStorage.getItem("profileTabId")), { 
+                              url: `https://www.instagram.com/direct/inbox/?id=${messageId}&message=${messageContent}&${urlParam}=true`,
+                              active: true}, function(tab) {
+                                tabId = tab.id;
+                              });
+                            }
+                            if(responsenewvalue.code === 1)
+                            {
+                              
+                              port.postMessage({userInfoDetails: responsenewvalue.payload.message,ThreadParams:paramsToSend,ConFlagBack:"DEFAULTMESSAGEBACK" });
+                              //localStorage.setItem("defaultMessageFromBackend",responsenewvalue.payload.message);
+                              var messageContent = responsenewvalue.payload.message;
+                              chrome.tabs.update( parseInt(localStorage.getItem("profileTabId")), { 
+                              url: `https://www.instagram.com/direct/inbox/?id=${messageId}&message=${messageContent}&${urlParam}=true`,
+                              active: true}, function(tab) {
+                                tabId = tab.id;
+                              });
+                            }
+
+                          }).catch(error=>{
+                          } )
+                        
+                  })
+                  .fail(function() { 
+                    // code for 404 error 
+                    console.log('Username was not found!')
+                  })
                 
-                let paramsToSend  =   {
-                  MfenevanId:GrammyWammyId,
-                  FacebookUserId:localStorage.getItem('instagramIdForTheLoggedInUser'),
-                  FriendFacebookId:instagramFriendId,
-                  FacebookFirstName:firstName,
-                  FacebookLastName:lastName,
-                  ProfileLink:profileLink,
-                  Username: postedByUsername,
-                  TimeNow:NowTime
+                } else {
+                  console.log('The username is invalid!')
                 }
-
-                console.log('Params To Send '+firstName);
-
-                let response  =  handleRequest(
-                "api/friend/checkFriendReadyToReciveDefaultMessage",
-                method.POST,
-                toJsonStr(paramsToSend)
-                ).then(async response =>  {
-                  let responsenewvalue = await response.json();
-                  console.log("Hit For Default",paramsToSend);
-                  console.log("Hit For Default Now Get From Backend",responsenewvalue.payload.message);
-                  console.log("The whole payload",responsenewvalue);
-                  
-                  if(responsenewvalue.code === 2)
-                  {
-                    localStorage.setItem("defaultMessageFromBackend",'Failed');
-                  }
-                  if(responsenewvalue.code === 1)
-                  {
-                    localStorage.setItem("defaultMessageFromBackend",responsenewvalue.payload.message);
-                    port.postMessage({userInfoDetails: responsenewvalue.payload.message,ThreadParams:paramsToSend,ConFlagBack:"DEFAULTMESSAGEBACK" });
-                  }
-
-                }).catch(error=>{
-                
-                  
-                } )
-                
-          }
-         
           
         }
     
